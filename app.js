6b2176089d44d34d5af4003824ff8af4e1a953ae
Artur Shum <ashum@stevens.edu>
2021-05-02 19:42:29 -0400

    app.js skeleton

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const configRoutes = require('./routes')
+const exphbs = require('express-handlebars');
+const static = express.static(__dirname + '/public');
+app.use('/public', static);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname:'handlebars'}));
+app.set('view engine', 'handlebars');
+app.use(
+    session({
+        name: 'AuthCookie',
+        secret: 'babababababababayoink',
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+configRoutes(app);
+
+app.listen(3000, () => console.log('Server up'));
+
+
