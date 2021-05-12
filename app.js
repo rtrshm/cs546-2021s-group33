786f0c4408676e806b0914fac072e8dcdcd33dcc
Christopher Tan <ctan5@stevens.edu>
2021-05-11 23:22:22 -0400

    Users can now sign up and log in
    Updated main.js for signup correctly updating user database, with appropriate error handling
    
    Updated app.js to allow users to successfully log in, with appropriate error handling
    
    Updated and created corresponding handlebars
    
    Co-Authored-By: jared-weinblatt <53191130+jared-weinblatt@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const session = require('express-session')
 const configRoutes = require('./routes')
 const exphbs = require('express-handlebars');
 const static = express.static(__dirname + '/public');
+const userDatabase = require('./data/users');
+const errorChecker = require('./data/errorChecker')
+const bcrypt = require('bcrypt');
+
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +22,56 @@ app.use(
         saveUninitialized: true
     })
 );
+
+app.post("/login", async (req, res) => {
+    let { username, password } = req.body;
+    if (!username || typeof(username) !== "string") {
+        res.render("signin.handlebars", {title: "Sign in failed", errormsg: "No username provided or username is not valid string."});
+    }
+    if (!password || typeof(password) !== "string") {
+        res.render("signin.handlebars", {title: "Sign in failed", errormsg: "No password provided or password is not valid string."});
+    }
+    username = username.toLowerCase();
+    let user;
+    try {
+        user = await userDatabase.findByUsername(username);
+    } catch(e){
+        res.render("signin.handlebars", {title: "Sign in failed", errormsg: "Incorrect username or password."});
+        return 0;
+    }
+
+    if (bcrypt.compareSync(password,user.password)) {
+        req.session.user = user;
+        let profile = {
+            perms: user.perms,
+            username: user.username,
+            dateJoined: user.dateJoined,
+            password: user.password,
+            usersFollowing: user.usersFollowing,
+            email: user.email,
+            favoriteGames: user.favoriteGames,
+            reviewsLeft: user.reviewsLeft
+        };
+        if (user.usersFollowing.length === 0) {
+            profile.usersFollowing = "This user is not following anyone.";
+        }
+        if (user.favoriteGames.length === 0) {
+            profile.favoriteGames = "This user has no favorite games.";
+        }
+        if (user.reviewsLeft.length === 0) {
+            profile.reviewsLeft = "This user has not left any reviews.";
+        }
+        res.render("profile.handlebars", {title: "User profile", object: profile});
+    }
+    else {
+        res.render("signin.handlebars", {title: "Sign in failed", errormsg: "Invalid username or password."});
+    }
+
+    if (!req.session.user) {
+        res.status(401);
+        res.render("posts/login", {title: "Error", errormsg: "Error: Did not provide a valid username and/or password."});
+    }
+});
 
 configRoutes(app);
 
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersDatabase = require("../data/users")
 const bcrypt = require('bcrypt');
+const errorChecker = require('../data/errorChecker')
 const saltRounds = 16;
 
 // const data = require("../data");
@@ -18,19 +19,42 @@ const saltRounds = 16;
     });
 
     router.post('/signup', async (req,res) => {
-        const {email, username, password } = req.body;
+        const {email, username, password, confirmpassword} = req.body;
+        if (!email) {
+            res.render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Email not provided or is not a valid string."});
+        }
+        try {
+            errorChecker.ValidateEmail(email);
+        } catch(e) {
+            res.render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Please enter a valid E-mail."});
+            return 0;   
+        }
+        if (!username || typeof(username) !== "string") {
+            res.render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Username not provided or is not a valid string."});
+            return 0;  
+        }
+        if (!password || typeof(password) !== "string") {
+            res.render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Password not provided or is not a valid string."});
+            return 0;  
+        }
+        if (!confirmpassword || typeof(confirmpassword) !== "string") {
+            res.render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Confirm password not provided or is not a valid string."});
+            return 0;  
+        }
+        if (password !== confirmpassword) {
+            res.render("signup.handlebars", {title: "Sign up failed", errormsg: "Error: Passwords do not match."});
+            return 0;  
+        }
+        const realusername = username.toLowerCase();
         const hashPassword = await bcrypt.hash(password, saltRounds);
         try {
-            let user = await usersDatabase.createUser("user", username, hashPassword, email);
-            
+            let user = await usersDatabase.createUser("user", realusername, hashPassword, email);
         } catch (e){
             res.render("signupError.handlebars", {title: "Sign up failed", error: e});
             return 0;
         }
-        res.render("signupSuccess.handlebars", {title: "Sign up success", user: username});
-        //let createUser = async (perms = "user", username, hashPassword, email) => {
-        
-        
+        res.render("signupSuccess.handlebars", {title: "Sign up success", user: realusername});
+        //let createUser = async (perms = "user", username, hashPassword, email) => { 
     });
 
     
