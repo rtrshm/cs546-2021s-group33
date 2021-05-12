4ec9cdf76eea915d7e68956b75601c2193879a07
Christopher Tan <ctan5@stevens.edu>
2021-05-11 22:00:19 -0400

    Updated signup route and added handlebars accordingly
    Created signup.handlebars, signupError.handlebars, and signupSuccess.handlebars.
    
    Modified signup route to construct user object and add user to the database
    
    Co-Authored-By: jared-weinblatt <53191130+jared-weinblatt@users.noreply.github.com>

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const usersDatabase = require("../data/users")
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
+
 // const data = require("../data");
 // const mongoCollections = require('../config/mongoCollections');
 // const booksData = mongoCollections.books;
@@ -11,6 +15,22 @@ const router = express.Router();
 
     router.get('/signup', async (req,res) => { 
         res.render("signup.handlebars", {title: "Sign up"})
+    });
+
+    router.post('/signup', async (req,res) => {
+        const {email, username, password } = req.body;
+        const hashPassword = await bcrypt.hash(password, saltRounds);
+        try {
+            let user = await usersDatabase.createUser("user", username, hashPassword, email);
+            
+        } catch (e){
+            res.render("signupError.handlebars", {title: "Sign up failed", error: e});
+            return 0;
+        }
+        res.render("signupSuccess.handlebars", {title: "Sign up success", user: username});
+        //let createUser = async (perms = "user", username, hashPassword, email) => {
+        
+        
     });
 
     
