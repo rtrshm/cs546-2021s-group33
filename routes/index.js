6e46bfb4a9c40bb52c80362a0216f09839415eba
Christopher Tan <ctan5@stevens.edu>
2021-05-12 21:43:28 -0400

    Fixed security issue with addgame and refactored codebase
    Code now checks if the user has admin permission when trying to add game.
    
    Changed route for addgame to /admin/addgame.
    
    Added dedicated route files for most routes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const profile = require('./profile');
 const main = require('./main');
 const games = require('./games');
+const nav = require('./nav');
+const admin = require('./admin');
+const quiz = require('./quiz');
+
 const constructorMethod = (app) => {
   app.use('/profile', profile);
   app.use('/games', games);
+  app.use('/nav', nav)
+  app.use('/admin', admin)
+  app.use('/quiz', quiz)
   app.use('/', main);
   app.use('*', (req, res) => {
     let url = `http://localhost:3000${req.baseUrl}`;
--- routes/profile.js
+++ routes/profile.js
@@ -1,34 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const errorChecker = require('../data/errorChecker');
 // const data = require("../data");
 // const mongoCollections = require('../config/mongoCollections');
 // const booksData = mongoCollections.books;
 // let { ObjectId } = require('mongodb')
 
-    router.get('/', async (req, res) => {
-//         <h1>Profile</h1>
-// <p>Permission group: {{object.perms}} </p>
-// <p>Username: {{object.username}}</p>
-// <p>Date joined: {{object.dateJoined}}</p>
-// <p>Users following: {{object.usersFollowing}}</p>
-// <p>Email: {{object.email}}</p>
-// <p>Favorite Games: {{object.favoriteGames}}</p>
-// <p>Reviews:{{object.reviewsLeft}}</p>
+router.get("/", async (req, res) => {
 
-        let obj = {
-            perms: "member",
-            username: "lempie",
-            dateJoined: "5/2/2021",
-            usersFollowing: "ecks",
-            email: "lempierox@gmail.com",
-            favoriteGames: ["Super Smash Brothers Melee", "Pokemon Showdown"],
-            reviewsLeft: ["Showdown is awesome! Just kidding it sucks I just got lucked!!! Crit ceit crit crit"]
-        };
-        res.render("profile.handlebars", {title: "Profile", object: obj})
-        
-    });
+    user = req.session.user;
 
+    if (!user.perms || typeof(user.perms) != 'string' || user.perms.trim().length == 0){
+        user.perms = 'N/A';
+    }
+
+    if (!user.username || typeof(user.username) != 'string' || user.username.trim().length == 0){
+        user.username = 'N/A';
+    }
+
+    if (!user.dateJoined || typeof(user.dateJoined) != 'string' || user.dateJoined.trim().length == 0){
+        user.dateJoined = 'N/A';
+    }
     
+    if (!user.email || typeof(user.email) != 'string' || user.email.trim().length == 0){
+        user.email = 'N/A';
+    }
 
+    try {
+        errorChecker.ValidateEmail(user.email)
+    }
+    catch (e){
+        user.email = 'N/A';
+    }
+
+    if (!user.usersFollowing || !Array.isArray(user.usersFollowing) || user.usersFollowing.length === 0) {
+        user.usersFollowing = "This user is not following anyone.";
+    }
+    if (!user.favoriteGames || !Array.isArray(user.favoriteGames) || user.favoriteGames.length === 0) {
+        user.favoriteGames = "This user has no favorite games.";
+    }
+    if (!user.reviewsLeft || !Array.isArray(user.reviewsLeft) || user.reviewsLeft.length === 0) {
+        user.reviewsLeft = "This user has not left any reviews.";
+    }
+    res.render("profile.handlebars", {title: "User profile", object: user});
+});
     
 module.exports = router;
